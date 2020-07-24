acb6dcf06ee33aab24990aca56ab7f0242534678
Mark Anklesaria <marknzl@outlook.co.nz>
2020-07-24 15:55:42 +1200

    Testing modification

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MSA 2020 DevOps :)
+          MSA 2020 DevOp :)
         </p>
         <a
           className="App-link"
